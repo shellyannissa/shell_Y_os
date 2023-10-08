61a1c7650bff523b01a300c2a75d8ef3dd932824
Shelly Anissa <shellyannissa29@gmail.com>
2023-10-08 19:31:58 +0530

    Stage 19 completed

---
+++ xfs-interface/Stage_19_assg1.sh
@@ -0,0 +1,22 @@
+load --os $HOME/myexpos/Stage_13/os_start_up.xsm
+load --idle $HOME/myexpos/Stage_12/idle.xsm
+load --init $HOME/myexpos/Stage_17/assg1.xsm
+load --exhandler $HOME/myexpos/Stage_19/exhandler.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --int=console $HOME/myexpos/Stage_16/cons_intr.xsm
+load --int=timer $HOME/myexpos/Stage_14/timer.xsm
+load --int=10 $HOME/myexpos/Stage_14/int_10.xsm
+load --int=6 $HOME/myexpos/Stage_16/int_6.xsm
+load --int=7 $HOME/myexpos/Stage_15/int_7.xsm
+load --int=9 $HOME/myexpos/Stage_19/int_9.xsm
+load --int=disk $HOME/myexpos/Stage_18/int_2.xsm
+load --module 7 $HOME/myexpos/Stage_19/module_7.xsm
+load --module 0 $HOME/myexpos/Stage_18/module_0.xsm
+load --module 4 $HOME/myexpos/Stage_18/module_4.xsm
+load --module 5 $HOME/myexpos/Stage_14/module_5.xsm
+load --module 1 $HOME/myexpos/Stage_19/module_1.xsm
+load --module 2 $HOME/myexpos/Stage_19/module_2.xsm
+rm odd_nos.xsm
+load --exec $HOME/myexpos/Stage_14/odd_nos.xsm
+rm assg1.xsm
+load --exec $HOME/myexpos/Stage_19/assg1.xsm
